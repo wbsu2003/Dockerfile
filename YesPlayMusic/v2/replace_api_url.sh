d739a2d211a6e9cb41f6081f37392da3594049d5
老苏 <wbsu2003@users.noreply.github.com>
2021-10-18 23:50:33 +0800

    Add files via upload

--- YesPlayMusic/v2/replace_api_url.sh
+++ YesPlayMusic/v2/replace_api_url.sh
@@ -2,7 +2,7 @@
 
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,netease_vanau,'"$VUE_APP_NETEASE_API_URL"',g' {} \;
 
-find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,netease_vaeau,'"$VUE_APP_ELECTRON_API_URL"',g' {} \;
+find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,netease_vaeaux,'"$VUE_APP_ELECTRON_API_URL"',g' {} \;
 
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,netease_vaeaud,'"$VUE_APP_ELECTRON_API_URL_DEV"',g' {} \;
 
