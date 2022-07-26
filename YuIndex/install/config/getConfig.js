e94b3eec1e4998cc8c58880dae82a3bbb4ea2aea
老苏 <wbsu2003@users.noreply.github.com>
2022-07-26 21:00:27 +0800

    Create getConfig.js

---
+++ YuIndex/install/config/getConfig.js
@@ -0,0 +1,14 @@
+module.exports = config;/**
+ * 获取当前环境的配置
+ * @author yupi
+ */
+let config;
+const env = process.env.NODE_ENV ?? "local";
+
+if (env === "local") {
+  config = require("./config");
+} else {
+  config = require(`./config.${env}`);
+}
+
+module.exports = config;
