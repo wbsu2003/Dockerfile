5b1441f7a83f28ec2eb8bb601d5cf2f36416ae2f
老苏 <wbsu2003@users.noreply.github.com>
2022-11-05 22:51:32 +0800

    Update run.sh

--- Dasherr/run.sh
+++ Dasherr/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ ! -f "/usr/share/nginx/html/settings.json" ];then
-	 echo "文件不存在"
-	 cp -r ./www/. /usr/share/nginx/html
+	echo "文件不存在"
+	cp -r ./www/. /usr/share/nginx/html
  else
-	 echo "文件存在"
+	echo "文件存在"
 fi
 
 nginx -g "daemon off;"
