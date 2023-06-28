3ee91cc5ee91a4ea8afed8580515802e9cb46fdf
老苏 <wbsu2003@users.noreply.github.com>
2023-06-28 15:34:00 +0800

    Update docker-entrypoint.sh

--- PHP-Dynamic-Qr-code/config/docker-entrypoint.sh
+++ PHP-Dynamic-Qr-code/config/docker-entrypoint.sh
@@ -2,4 +2,6 @@
 
 find '/etc/apache2' -name '*.conf' -exec sed -i -e 's,localhost,'"$WEB_URL"',g' {} \;
 
+find '/var/www/html' -name 'read.php' -exec sed -i -e 's,qrcode/config,'"config"',g' {} \;  
+
 exec apache2-foreground
